4683ce08d906277e6110d20746ab716bbf6f8061
Steve Marshall <steve@stevemarshall.com>
2021-09-03 22:32:41 +0100

    Allow files and folders to be imported and listed
    Folders need a distinct UI mode, so we're offering distinct buttons for
    the two modes.

--- NSMetadataQueryUbiquitousExternalDocumentsTest/NSMetadataQueryUbiquitousExternalDocumentsTestApp.swift
+++ NSMetadataQueryUbiquitousExternalDocumentsTest/NSMetadataQueryUbiquitousExternalDocumentsTestApp.swift
@@ -1,10 +1,67 @@
 import SwiftUI
+import UniformTypeIdentifiers
 
 @main
 struct NSMetadataQueryUbiquitousExternalDocumentsTestApp: App {
+    @State private var adding: AddMode? = nil
+    @State private var addedItems = [URL]()
+
     var body: some Scene {
         WindowGroup {
-            Text("NSMetadataQueryUbiquitousExternalDocumentsTest")
+            NavigationView {
+                List {
+                    Section("Added items") {
+                        ForEach(addedItems, id: \.absoluteString) { added in
+                            Text(added.lastPathComponent)
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button(action: { adding = .file }, label: {
+                            Label("Add file", systemImage: "doc.badge.plus")
+                        })
+                        Button(action: { adding = .folder }, label: {
+                            Label("Add folder", systemImage: "folder.badge.plus")
+                        })
+                    }
+                }
+                .fileImporter(
+                    isPresented: Binding(
+                        get: { adding != nil },
+                        set: { _ in adding = nil }
+                    ),
+                    allowedContentTypes: adding?.allowedContentTypes ?? [],
+                    allowsMultipleSelection: true,
+                    onCompletion: importFiles
+                )
+                .navigationTitle("MetadataQuery Test")
+            }
+        }
+    }
+}
+
+extension NSMetadataQueryUbiquitousExternalDocumentsTestApp {
+    func importFiles(_ result: Result<[URL], Error>) {
+        guard case .success(let urls) = result else {
+            return
+        }
+        addedItems.append(
+            contentsOf: urls
+        )
+    }
+}
+
+extension NSMetadataQueryUbiquitousExternalDocumentsTestApp {
+    private enum AddMode {
+        case folder
+        case file
+
+        var allowedContentTypes: [UTType] {
+            switch self {
+            case .folder: return [.folder]
+            case .file: return [.content]
+            }
         }
     }
 }
